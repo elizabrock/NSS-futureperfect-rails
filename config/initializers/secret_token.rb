ad15a6b336a7be3188e7509d05bb332a645c8a57
Eliza Brock <eliza@elizabrocksoftware.com>
2013-06-11 11:16:21 -0500

    rails new . --database=postgresql --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NSSFutureperfectRails::Application.config.secret_token = 'e7908378e56ccef6c54ed3eb415753185ef7bedac5344774aa5551fb456d7e937751c5cd78cc665283b21d012ada90c52d707206f432bcb390dbc1f39f549b4a'
