31be76dd9d3d18023840fdf1ed92e29424b39d03
Eliza Brock <eliza@elizabrocksoftware.com>
2013-06-15 11:40:55 -0500

    User signs up

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -3,6 +3,14 @@ Given(/^the user "(.*?)" with password "(.*?)"$/) do |email, password|
   refute user.new_record?
 end
 
+Then(/^I should be able to log back in as "(.*?)" with password "(.*?)"$/) do |email, password|
+  visit new_user_session_path
+  fill_in "Email", with: email
+  fill_in "Password", with: password
+  click_button "Sign in"
+  page.should have_content("You are now signed in.")
+end
+
 Given "I am logged in" do
   user = User.create( email: "joe@example.com", password: "password")
   visit new_user_session_path
